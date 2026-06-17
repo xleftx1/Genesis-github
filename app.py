b436204e5826baa08b7d301f495031c33b0057b9
xleftx1 <roqueildefonsoedurdo@gmail.com>
2026-06-17 10:26:57 -0500

    Actualización de la página de servicios

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def contacto():
 @app.route("/servicios")
 def servicios():
     return render_template("servicios.html")
+@app.route("/corte")
+def corte():
+    return render_template("corte.html")
+
+@app.route("/tinturado")
+def tinturado():
+    return render_template("tinturado.html")
+
+@app.route("/peinado")
+def peinado():
+    return render_template("peinado.html")
 
 if __name__ == "__main__":
     app.run(debug=True);
